7b660d0ac272b4a424bdde182cdc5f5e15066877
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-11-19 18:18:45 +0100

    Class for ip-validation

---
+++ src/IpValidation/IpValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EVB\IpValidation;
+
+use Anax\Commons\ContainerInjectableInterface;
+use Anax\Commons\ContainerInjectableTrait;
+
+/**
+ * Simple class for validating IPv4 and IPv6 addresses.
+ */
+class IpValidator implements ContainerInjectableInterface
+{
+    use ContainerInjectableTrait;
+
+    const IPv4Regex = "^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";
+    const IPv6Regex = "^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$";
+
+    /**
+     * Validates an IPv4 address.
+     *
+     * @param string $ip
+     * @return boolean
+     */
+    public function validateIPv4(string $ip) : bool
+    {
+        return (bool)(\preg_match($this::IPv4Regex, $ip));
+    }
+
+    /**
+     * Validates an IPv6 address.
+     *
+     * @param string $ip
+     * @return boolean
+     */
+    public function validateIPv6(string $ip) : bool
+    {
+        return (bool)(\preg_match($this::IPv6Regex, $ip));
+    }
+}
