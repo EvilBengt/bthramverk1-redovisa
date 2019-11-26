426660fa3d217436c3ab033c8659b5a2e081c0c5
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-11-26 15:37:56 +0100

    More model classes

--- src/IpValidation2/IpLocator.php
+++ src/IpValidation2/IpLocator.php
@@ -2,12 +2,38 @@
 
 namespace EVB\IpValidation2;
 
+use EVB\IpValidation2\CurlWrapper;
+
 
 /**
  * Simple class for locating IP-addresses.
  */
 class IpLocator
 {
+    private const IPSTACK_BASE_URL = "http://api.ipstack.com/";
+    private $ipstackApiKey;
+
+    private $curl;
+
+    /**
+     * Configure the IpLocator.
+     *
+     * @param array $config
+     * @return void
+     */
+    public function configure(array $config, CurlWrapper $curl) : void
+    {
+        $this->ipstackApiKey = $config["items"][0]["config"]["api_key"];
+        $this->curl = $curl;
+    }
+
+    /**
+     * Gets the domain name for the supplied IP-address.
+     * Returns "" (empty string) if no domain found.
+     *
+     * @param string $ip
+     * @return string
+     */
     public function getDomainName(string $ip) : string
     {
         $domain = \gethostbyaddr($ip);
@@ -17,5 +43,26 @@ class IpLocator
         }
 
         return $domain ? $domain : "";
+    }
+
+    /**
+     * Gets geographical information from ipstack for the supplied IP-address.
+     *
+     * @param string $ip
+     * @return array
+     */
+    public function getGeoInfo(string $ip) : array
+    {
+        $url = self::IPSTACK_BASE_URL . $ip . "?access_key=" . $this->ipstackApiKey;
+
+        $response = \json_decode($this->curl->fetch($url), true);
+
+        return $response["continent_name"] ? [
+            "continent" => $response["continent_name"],
+            "country" => $response["country_name"],
+            "region" => $response["region_name"],
+            "city" => $response["city"],
+            "zip" => $response["zip"]
+        ] : [];
     }
 }
