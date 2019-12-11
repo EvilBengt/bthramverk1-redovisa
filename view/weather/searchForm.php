08750ad5d6cc8004d90895ecb57863a771e644bb
Anton 'Evil_Bengt' Johnsson Håkansson <anton.jh@hotmail.com>
2019-12-11 19:19:03 +0100

    Old search params as default values + reorganized stuff + reset link

--- view/weather/searchForm.php
+++ view/weather/searchForm.php
@@ -4,32 +4,33 @@ namespace Anax\View;
 
 ?><h2>Sök väder</h2>
 
-<form class="form" action="<?= url("weather") ?>">
+<form class="form" action="<?= url("weather") ?>" id="weatherForm">
     <p>
         Skriv in koordinater...
     </p>
     <label>
         Latitud:<br>
-        <input class="input" type="text" name="lat">
+        <input class="input" type="text" name="lat" value="<?= $search["lat"] ?>">
     </label>
     <br>
     <label>
         Longitud:<br>
-        <input class="input" type="text" name="long">
+        <input class="input" type="text" name="long" value="<?= $search["long"] ?>">
     </label>
     <p>
         ...eller en IP-adress för att söka.
     </p>
     <label>
         IP-adress:<br>
-        <input class="input" type="text" name="ip">
+        <input class="input" type="text" name="ip" value="<?= $search["ip"] ?>">
     </label>
     <br>
-    <br>
-    <label>
-        <input type="checkbox" name="useExample">
-        Använd exempel-väder för att spara på API-förfrågningar.
-    </label>
-    <br>
-    <button class="button" type="submit">Sök</button>
+    <button class="button" type="submit" name="search" value="normal">
+        Sök
+    </button>
+    <button class="button" type="submit" name="search" value="example"
+        title="Använder sparad väderdata för att spara på API-förfrågningar.">
+        Sök med exempel-väder
+    </button>
+    <a class="button" href="<?= url("weather") ?>">Rensa</a>
 </form>
